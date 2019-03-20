b2c27c60add122b7716f08d5d00d7fc195f177a1
Sean Doyle <sdoylelambda@gmail.com>
2019-03-20 16:57:27 -0400

    TodoList.js list added.

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,17 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react';
+
+const list = [
+    {
+        task: 'Organize Garage',
+        id: 1528817077286,
+        completed: false
+      },
+      {
+        task: 'Bake Cookies',
+        id: 1528817084358,
+        completed: false 
+    }
+]
