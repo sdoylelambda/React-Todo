fd3955c02ed3e7e76b264ae0c611d204b9238f12
Sean Doyle <sdoylelambda@gmail.com>
2019-03-21 17:08:14 -0400

    Test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TodoForm from './components/TodoComponents/TodoForm';
 let id = 0
 
 class App extends React.Component {
-  // you will need a place to store your state in this component...
+  // you will need a place to store your state in this component....
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
